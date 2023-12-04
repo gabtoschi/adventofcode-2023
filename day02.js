7adb38635c46550cc42019f35e8e8a0ae75ad6c9
Gabriel Toschi <gabriel.toschi@hotmail.com>
2023-12-03 23:50:54 -0300

    Day 2: Cube Conundrum 🌟🌟 - now in JS

--- day02.js
+++ day02.js
@@ -1,24 +1,14 @@
-import * as aoc from './utils/aoc';
-import input from './inputs/input02';
+const aoc = require('./utils/aoc');
+const input = require('./inputs/input02');
 
-type Subset = {
-  red: number;
-  blue: number;
-  green: number;
-}
-type Game = {
-  id: number;
-  subsets: Subset[];
-}
-
-const data: string[] = aoc.lines(input);
-const games: Game[] = data.map(line => {
+const data = aoc.lines(input);
+const games = data.map(line => {
   const [gameData, subsetData] = line.split(': ');
   const [_, gameId] = gameData.split(' ');
   const subsetStrings = subsetData.split('; ');
 
-  const subsets: Subset[] = subsetStrings.map(str => {
-    const mappedSubset: Subset = { red: 0, blue: 0, green: 0 };
+  const subsets = subsetStrings.map(str => {
+    const mappedSubset = { red: 0, blue: 0, green: 0 };
     const colorStrings = str.split(', ');
 
     colorStrings.forEach(colorStr => {
@@ -36,7 +26,7 @@ const games: Game[] = data.map(line => {
 });
 
 // STAR 01
-const checkGamesByMaxCubes = (games: Game[], max: Subset): number => {
+const checkGamesByMaxCubes = (games, max) => {
   let idSum = 0;
 
   games.forEach(game => {
@@ -55,11 +45,11 @@ const checkGamesByMaxCubes = (games: Game[], max: Subset): number => {
 aoc.answer(1, checkGamesByMaxCubes(games, { red: 12, green: 13, blue: 14 }));
 
 // STAR 02
-const getMinPower = (games: Game[]): number =>  {
+const getMinPower = (games) =>  {
   let powerSum = 0;
 
   games.forEach(game => {
-    const minCubes: Subset = { red: 0, green: 0, blue: 0 };
+    const minCubes = { red: 0, green: 0, blue: 0 };
 
     game.subsets.forEach(subset => {
       if (subset.red > minCubes.red) minCubes.red = subset.red;
